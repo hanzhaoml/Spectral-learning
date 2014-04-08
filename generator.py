68fc14d3f8a852da63dc66887737c3382b3f4fff
KeiraZhao <zhaohan2009011312@gmail.com>
2014-04-08 01:47:19 -0400

    generate more data

--- generator.py
+++ generator.py
@@ -12,32 +12,22 @@ import numpy as np
 
 from hmm import HMM
 
-if __name__ == '__main__':    
-    usage = '''./generator.py file m n training_seq_length testing_seq_length
-            file is the filepath to store the data 
-            m is the size of states in HMM    
-            n is the size of observations in HMM  
-            dsize is the size of data you\'d like to generate    
-            sqlen is the maximal length of each observation sequence.\n'''
-    if len(sys.argv) < 6:
-        sys.stderr.write(usage)
-        exit()
-    
+def generate(m, n, dsize, tsize, filename):
     t_start = time.clock()
     
     # Create Hidden Markov Model randomly. 
     # m --- The number of hidden states
     # n --- The number of observation states
-    model = HMM(int(sys.argv[2]), int(sys.argv[3]))
+    model = HMM(m, n)
 
     # Generate training set and testing set
-    training_seq = model.generate_train_data(int(sys.argv[4]))
+    training_seq = model.generate_train_data(dsize)
 #     test_seqs = model.generate_test_data(int(sys.argv[5]))
-    test_seqs = model.gendata(int(sys.argv[5]), 50)
+    test_seqs = model.gendata(tsize, 50)
     
-    training_filename = "train_" + sys.argv[1] + ".data"
-    test_filename = "test_" + sys.argv[1] + ".data"
-    model_filename = "model_" + sys.argv[1] + ".npy"
+    training_filename = "train_" + filename + ".data"
+    test_filename = "test_" + filename + ".data"
+    model_filename = "model_" + filename + ".npy"
     
     np.savetxt(training_filename, [training_seq], delimiter=",", fmt="%d")
 #     np.savetxt(test_filename, test_seqs, delimiter=",", fmt="%d")
@@ -50,4 +40,25 @@ if __name__ == '__main__':
     print 'Time used:', (t_end - t_start), 'seconds'
     
     # Save model parameters
-    HMM.to_file(model_filename, model)
+    HMM.to_file(model_filename, model)    
+    
+    
+def regenerate(hmm, training_filename, dsize):
+    training_seq = hmm.generate_train_data(dsize)
+    np.savetxt(training_filename, [training_seq], delimiter=",", fmt="%d")
+    
+if __name__ == '__main__':    
+    usage = '''./generator.py file m n training_seq_length testing_seq_length
+            file is the filepath to store the data 
+            m is the size of states in HMM    
+            n is the size of observations in HMM  
+            dsize is the size of data you\'d like to generate    
+            sqlen is the maximal length of each observation sequence.\n'''
+#     if len(sys.argv) < 6:
+#         sys.stderr.write(usage)
+#         exit()
+    if len(sys.argv) < 4:
+        sys.stderr("Need more parameters")
+        exit()
+    hmm = HMM.from_file(sys.argv[1])
+    regenerate(hmm, sys.argv[2], int(sys.argv[3]))
